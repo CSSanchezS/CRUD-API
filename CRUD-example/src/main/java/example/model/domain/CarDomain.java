2c0e155d1d54033ecb451bcecd82b7058416bda4
CSSanchezS <carlosomarsanchezsolorzano@gmail.com>
2020-01-10 16:15:26 -0600

    settings

---
+++ CRUD-example/src/main/java/example/model/domain/CarDomain.java
@@ -0,0 +1,12 @@
+package example.model.domain;
+
+import lombok.Data;
+
+@Data
+public class CarDomain {
+	public int idCar;
+    public String name;
+    public String version;
+    public String model;
+
+}
