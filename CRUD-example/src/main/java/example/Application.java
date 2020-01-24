6ca53c03cd95d7a813c602dcb72e8f6942099339
CSSanchezS <carlosomarsanchezsolorzano@gmail.com>
2020-01-24 11:33:42 -0600

    fixed

--- CRUD-example/src/main/java/example/Application.java
+++ CRUD-example/src/main/java/example/Application.java
@@ -16,12 +16,7 @@ public class Application {
 	public static void main(String[] args)throws IOException {
 		// TODO Auto-generated method stub
 		SpringApplication.run(example.Application.class, args);
-		//openHomePage(); //with this line call automatically the web service
+		
 	}
 	
-	private static void openHomePage() throws IOException {
-	       Runtime rt = Runtime.getRuntime();
-	       rt.exec("rundll32 url.dll,FileProtocolHandler " + "http://localhost:8080/get");
-	    }
-
 }
